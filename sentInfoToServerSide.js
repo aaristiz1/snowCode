b0f215d99f2bf1a38933fa4f961b62395e889414
aaristiz1 <31866911+aaristiz1@users.noreply.github.com>
2020-03-05 10:48:57 -0500

    Create sentInfoToServerSide.js
    Code to make a request to the server-side and also to send information to be processed in the server

---
+++ sentInfoToServerSide.js
@@ -0,0 +1,12 @@
+//Client Side Code
+c.server.get( {action_name : 'validate', parent_id:  $location.search().sys_id, table: $location.search().table  } ).then(function(r) {      
+   
+});
+
+
+//Server Side Code
+if (input) {
+
+    if (input.action_name == 'validate') {
+    }
+  }
